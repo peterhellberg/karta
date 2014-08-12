c7a2867086ca624bae62fed46d5334490c3f61b7
Peter Hellberg <peter@c7.se>
2014-08-12 22:31:17 +0200

    Set cell colors based on distance from center

--- karta/karta.go
+++ karta/karta.go
@@ -2,9 +2,12 @@ package karta
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
+	"log"
 	"math"
+	"math/rand"
 	"os"
 
 	"github.com/peterhellberg/karta/palette"
@@ -41,8 +44,31 @@ func NewDiagram(w, h float64, c, r int) *voronoi.Diagram {
 	return diagram
 }
 
+type CellPrefs struct {
+	Index            int
+	DistanceToCenter float64
+	FillColor        color.RGBA
+	StrokeColor      color.RGBA
+}
+
 // DrawDiagramImage draws a Voroni diagram to an image
 func DrawDiagramImage(diagram *voronoi.Diagram, w, h int) image.Image {
+	f, _ := os.OpenFile("karta.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0640)
+	defer f.Close()
+	log.SetOutput(f)
+
+	unit := float64((w + h) / 20)
+
+	allPrefs := make(map[int]CellPrefs)
+	centerOfMap := voronoi.Vertex{float64(w / 2), float64(h / 2)}
+
+	for i, cell := range diagram.Cells {
+		allPrefs[i] = CellPrefs{
+			Index:            i,
+			DistanceToCenter: utils.Distance(cell.Site, centerOfMap),
+		}
+	}
+
 	img := image.NewRGBA(image.Rect(0, 0, w, h))
 
 	draw.Draw(img, img.Bounds(), &image.Uniform{palette.Darkblue}, image.ZP, draw.Src)
@@ -51,12 +77,74 @@ func DrawDiagramImage(diagram *voronoi.Diagram, w, h int) image.Image {
 	p.SetFillColor(palette.Green)
 
 	l := draw2d.NewGraphicContext(img)
-	l.SetLineWidth(2)
-	l.SetStrokeColor(palette.Purple)
+
+	var tiny float64
+
+	switch {
+	case h <= 256:
+		tiny = 1
+	case h <= 512:
+		tiny = 2
+	default:
+		tiny = 3
+	}
+
+	l.SetLineWidth(tiny)
 
 	// Iterate over cells
-	for _, cell := range diagram.Cells {
-		l.SetFillColor(palette.Pink)
+	for i, cell := range diagram.Cells {
+		prefs := allPrefs[i]
+
+		d := prefs.DistanceToCenter
+
+		switch {
+		case d < unit*1.4 && rand.Intn(4) < 3:
+			prefs.FillColor = palette.Darkergreen
+			prefs.StrokeColor = palette.Darkgreen
+		case d < unit*3.4:
+			prefs.FillColor = palette.Green
+			prefs.StrokeColor = palette.Darkgreen
+		case d < unit*4.2 && rand.Intn(4) < 3:
+			prefs.FillColor = palette.Blue
+			prefs.StrokeColor = palette.Darkerblue
+		default:
+			prefs.FillColor = palette.Darkblue
+			prefs.StrokeColor = palette.Darkerblue
+		}
+
+		// Make sure left and right edges of the map are deep water
+		if cell.Site.X < unit || cell.Site.X > float64(w)-unit {
+			if prefs.FillColor == palette.Green {
+				prefs.FillColor = palette.Blue
+				prefs.StrokeColor = palette.Darkerblue
+			}
+
+			if prefs.FillColor != palette.Blue {
+				prefs.FillColor = palette.Darkblue
+				prefs.StrokeColor = palette.Darkerblue
+			}
+		}
+
+		// Make sure top and bottom edges of the map are deep water
+		if cell.Site.Y < unit/1.2 || cell.Site.Y > float64(h)-unit/1.2 {
+			if prefs.FillColor != palette.Blue {
+				prefs.FillColor = palette.Darkblue
+				prefs.StrokeColor = palette.Darkerblue
+			}
+
+			if prefs.FillColor == palette.Green {
+				prefs.FillColor = palette.Blue
+				prefs.StrokeColor = palette.Darkerblue
+			}
+		}
+
+		if cell.Site.Y < unit/3 || cell.Site.Y > float64(h)-unit/3 {
+			prefs.FillColor = palette.Darkblue
+			prefs.StrokeColor = palette.Darkerblue
+		}
+
+		l.SetFillColor(prefs.FillColor)
+		l.SetStrokeColor(prefs.StrokeColor)
 
 		for _, hedge := range cell.Halfedges {
 			a := hedge.GetStartpoint()
@@ -68,7 +156,7 @@ func DrawDiagramImage(diagram *voronoi.Diagram, w, h int) image.Image {
 
 		l.FillStroke()
 
-		p.ArcTo(cell.Site.X, cell.Site.Y, 3, 3, 0, τ)
+		p.ArcTo(cell.Site.X, cell.Site.Y, tiny*1.4, tiny*1.4, 0, τ)
 		p.FillStroke()
 	}
 
