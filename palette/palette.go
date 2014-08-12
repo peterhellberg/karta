4773d3a57c32cac5a71d15406a5f9e03219a43a0
Peter Hellberg <peter@c7.se>
2014-08-12 23:37:43 +0200

    Moved comments to the bottom

--- palette/palette.go
+++ palette/palette.go
@@ -3,34 +3,30 @@ package palette
 import "image/color"
 
 var (
-	// Green http://www.colourlovers.com/color/D1E751/limey
-	Green       = color.RGBA{0xD1, 0xE7, 0x51, 0xFF}
-	Darkgreen   = color.RGBA{0x92, 0xA1, 0x38, 0xFF}
-	Darkergreen = color.RGBA{0x68, 0x73, 0x28, 0xFF}
-
-	// Gray http://www.colourlovers.com/color/B8B6B6/boring
-	Gray = color.RGBA{0xB8, 0xB6, 0xB6, 0xFF}
-
-	// Black http://www.colourlovers.com/color/000000/Black
-	Black = color.RGBA{0x00, 0x00, 0x00, 0xFF}
-
-	// White http://www.colourlovers.com/color/FFFFFF/white
-	White = color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}
-
-	// Blue http://www.colourlovers.com/color/4DBCE9/electric_blue
-	Blue      = color.RGBA{0x4D, 0xBC, 0xE9, 0xFF}
-	Beachblue = color.RGBA{0x73, 0xC8, 0xBE, 0xFF}
-
-	// Darkblue http://www.colourlovers.com/color/26ADE4/blube
-	Darkblue   = color.RGBA{0x26, 0xAD, 0xE4, 0xFF}
-	Darkerblue = color.RGBA{0x1e, 0x8a, 0xb6, 0xFF}
-
-	// Orange http://www.colourlovers.com/color/FF6600/Rounge
-	Orange = color.RGBA{0xFF, 0x66, 0x00, 0xFF}
-
-	// Pink http://www.colourlovers.com/color/F20765/Dont_Stop_Loving_Me
-	Pink = color.RGBA{0xF2, 0x07, 0x65, 0xFF}
-
-	// Purple http://www.colourlovers.com/color/850841/complaining_violet
-	Purple = color.RGBA{0x85, 0x08, 0x41, 0xFF}
+	Green        = color.RGBA{0xD1, 0xE7, 0x51, 0xFF}
+	Darkgreen    = color.RGBA{0x92, 0xA1, 0x38, 0xFF}
+	Darkergreen  = color.RGBA{0x68, 0x73, 0x28, 0xFF}
+	Darkestgreen = color.RGBA{0x53, 0x5C, 0x20, 0xFF}
+	Gray         = color.RGBA{0xB8, 0xB6, 0xB6, 0xFF}
+	Black        = color.RGBA{0x00, 0x00, 0x00, 0xFF}
+	White        = color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}
+	Blue         = color.RGBA{0x4D, 0xBC, 0xE9, 0xFF}
+	Beachblue    = color.RGBA{0x73, 0xC8, 0xBE, 0xFF}
+	Darkblue     = color.RGBA{0x26, 0xAD, 0xE4, 0xFF}
+	Darkerblue   = color.RGBA{0x22, 0x9B, 0xCD, 0xFF}
+	Orange       = color.RGBA{0xFF, 0x66, 0x00, 0xFF}
+	Pink         = color.RGBA{0xF2, 0x07, 0x65, 0xFF}
+	Purple       = color.RGBA{0x85, 0x08, 0x41, 0xFF}
 )
+
+// Inspiration
+//
+// Gray http://www.colourlovers.com/color/B8B6B6/boring
+// Black http://www.colourlovers.com/color/000000/Black
+// White http://www.colourlovers.com/color/FFFFFF/white
+// Blue http://www.colourlovers.com/color/4DBCE9/electric_blue
+// Darkblue http://www.colourlovers.com/color/26ADE4/blube
+// Orange http://www.colourlovers.com/color/FF6600/Rounge
+// Pink http://www.colourlovers.com/color/F20765/Dont_Stop_Loving_Me
+// Purple http://www.colourlovers.com/color/850841/complaining_violet
+// Green http://www.colourlovers.com/color/D1E751/limey
