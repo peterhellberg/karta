3068b41a57625b4694422dfa7d85ed0a2457a2e0
Peter Hellberg <peter@c7.se>
2014-09-06 16:09:09 +0200

    Switched to gl 2.1, and from Color4f to Color4ub

--- cmd/karta-gui/extensions.go
+++ cmd/karta-gui/extensions.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"gopkg.in/qml.v1"
-	"gopkg.in/qml.v1/gl/2.0"
+	"gopkg.in/qml.v1/gl/2.1"
 )
 
 func init() {
 	qml.RegisterTypes("GoExtensions", 1, 0, []qml.TypeSpec{{
-		Init: func(r *GoRect, obj qml.Object) { r.Object = obj },
+		Init: func(r *GoRect, obj qml.Object) {
+			r.Object = obj
+		},
 	}})
 }
 
@@ -23,15 +25,17 @@ func (r *GoRect) Paint(p *qml.Painter) {
 
 	gl.Enable(GL.BLEND)
 	gl.BlendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
-	gl.Color4f(1.0, 1.0, 1.0, 0.8)
+	gl.Color4ub(0xff, 0x66, 0x00, 0xaa)
 	gl.Begin(GL.QUADS)
 	gl.Vertex2f(0, 0)
 	gl.Vertex2f(width, 0)
 	gl.Vertex2f(width, height)
 	gl.Vertex2f(0, height)
 	gl.End()
+
 	gl.LineWidth(2.5)
 	gl.Color4f(0.0, 0.0, 0.0, 1.0)
+	gl.Color4ub(0xff, 0x66, 0x00, 0xff)
 	gl.Begin(GL.LINES)
 	gl.Vertex2f(0, 0)
 	gl.Vertex2f(width, height)
