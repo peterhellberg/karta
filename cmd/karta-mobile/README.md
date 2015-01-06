39565fa35fb8e89a2317bc477bb94f8df43af5c7
Peter Hellberg <peter@c7.se>
2015-01-06 23:15:13 +0100

    Initial karta-mobile

---
+++ cmd/karta-mobile/README.md
@@ -0,0 +1,3 @@
+# karta-mobile
+
+Experimental GUI using the [golang.org/x/mobile](https://godoc.org/golang.org/x/mobile) packages.
---
+++ cmd/karta-mobile/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"image"
+	"log"
+	"time"
+
+	"github.com/peterhellberg/karta"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event"
+	"golang.org/x/mobile/f32"
+	"golang.org/x/mobile/sprite"
+	"golang.org/x/mobile/sprite/clock"
+	"golang.org/x/mobile/sprite/glsprite"
+)
+
+var (
+	start     = time.Now()
+	lastClock = clock.Time(-1)
+	eng       = glsprite.Engine()
+	scene     *sprite.Node
+	kn        *sprite.Node
+)
+
+func main() {
+	app.Run(app.Callbacks{
+		Draw: func() {
+			if scene == nil {
+				loadScene(40)
+			}
+
+			now := clock.Time(time.Since(start) * 60 / time.Second)
+			if now == lastClock {
+				return
+			}
+			lastClock = now
+
+			eng.Render(scene, now)
+		},
+		Touch: func(e event.Touch) {
+			if e.Type != event.TouchStart {
+				return
+			}
+
+			eng.SetSubTex(kn, loadTexture((int(e.Loc.X) * (int(e.Loc.Y/25) + 1))))
+		},
+	})
+}
+
+func newNode() *sprite.Node {
+	n := &sprite.Node{}
+	eng.Register(n)
+	scene.AppendChild(n)
+
+	return n
+}
+
+func loadScene(count int) {
+	kn = newNode()
+	scene = &sprite.Node{}
+
+	eng.Register(scene)
+	eng.SetSubTex(kn, loadTexture(count))
+	eng.SetTransform(kn, f32.Affine{
+		{212, 0, 0},
+		{0, 212, 0},
+	})
+}
+
+func loadTexture(count int) sprite.SubTex {
+	t, err := eng.LoadTexture(karta.Image(512, 512, count, 4))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return sprite.SubTex{t, image.Rect(0, 0, 512, 512)}
+}
